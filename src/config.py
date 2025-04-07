57f93a4029c776eb830601cde4bec16d9cfa90f4
taotieren <admin@taotieren.com>
2025-04-07 14:21:59 +0800

    Update logs are exported to the system path

--- src/config.py
+++ src/config.py
@@ -1,18 +1,30 @@
 import os
+import sys
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtCore import QSettings
+from PyQt6.QtCore import QSettings, QStandardPaths
 
-USER_CONFIG_INI = 'user.ini'
+APP_NAME = "nxp-uuu-qt-tool"
+
+if sys.platform == "win32":
+    config_dir = QStandardPaths.writableLocation(QStandardPaths.AppDataLocation)
+else:
+    config_dir = os.path.expanduser("~/.config")
+
+config_path = os.path.join(config_dir, APP_NAME, "user.ini")
+
+os.makedirs(os.path.dirname(config_path), exist_ok=True)
+
+USER_CONFIG_INI = config_path
 
 configini = QSettings(USER_CONFIG_INI, QSettings.Format.IniFormat)
 
 config_param = {
-    "fastboot": '',
-    "uboot": '',
-    "kernel": '',
-    "devicetree": '',
-    "rootfs": '',
-    "file": '',
+    "fastboot": "",
+    "uboot": "",
+    "kernel": "",
+    "devicetree": "",
+    "rootfs": "",
+    "file": "",
 }
 
 
@@ -25,8 +37,9 @@ def configCreate():
     for key, value in config_param.items():
         configini.setValue(key, value)
 
+
 def init():
-    if os.path.exists(f'{USER_CONFIG_INI}'):
+    if os.path.exists(f"{USER_CONFIG_INI}"):
         configLoad()
     else:
         configCreate()
